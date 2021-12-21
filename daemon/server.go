d85c8aeb2dfe00c526e58c25fd83308a0393bfb9
Anton Novojilov <andy@essentialkaos.com>
2021-12-22 02:38:43 +0300

    Refactoring

--- daemon/server.go
+++ daemon/server.go
@@ -51,14 +51,6 @@ func startHTTPServer(ip, port string) error {
 // fastHTTPHandler is handler for all requests
 func fastHTTPHandler(ctx *fasthttp.RequestCtx) {
 	defer requestRecover(ctx)
-
-	path := string(ctx.Path())
-
-	if path != "/status.svg" {
-		ctx.SetStatusCode(404)
-		return
-	}
-
 	statusHandler(ctx)
 }
 
@@ -68,7 +60,8 @@ func requestRecover(ctx *fasthttp.RequestCtx) {
 
 	if r != nil {
 		log.Error("Recovered internal error in HTTP request handler: %v", r)
-		ctx.SetStatusCode(501)
+		configureResponseHeaders(ctx)
+		ctx.Write(svg.Empty)
 	}
 }
 
@@ -76,33 +69,46 @@ func requestRecover(ctx *fasthttp.RequestCtx) {
 
 // statusHandler is status request handler
 func statusHandler(ctx *fasthttp.RequestCtx) {
+	configureResponseHeaders(ctx)
+
+	path := string(ctx.Path())
+
+	if path != "/status.svg" {
+		ctx.Write(svg.Empty)
+		return
+	}
+
 	query := ctx.QueryArgs()
 
 	if !query.Has(QUERY_MAIL) || !query.Has(QUERY_TOKEN) {
-		ctx.SetStatusCode(404)
+		ctx.Write(svg.Empty)
 		return
 	}
 
 	if !bytes.Equal(token, query.Peek(QUERY_TOKEN)) {
-		ctx.SetStatusCode(404)
+		ctx.Write(svg.Empty)
 		return
 	}
 
+	ctx.Write(getStatusBadge(query.Peek(QUERY_MAIL)))
+}
+
+// configureResponseHeaders configures response headers and status code
+func configureResponseHeaders(ctx *fasthttp.RequestCtx) {
 	ctx.Response.Header.Set("Content-Type", "image/svg+xml")
 	ctx.Response.Header.Set("Cache-Control", "no-cache, no-store, must-revalidate")
 	ctx.Response.Header.Set("Pragma", "no-cache")
 	ctx.Response.Header.Set("Expires", "0")
-
-	ctx.WriteString(getStatusBadge(string(query.Peek(QUERY_MAIL))))
-
 	ctx.SetStatusCode(200)
 }
 
 // getStatusBadge returns status badge
-func getStatusBadge(mail string) string {
+func getStatusBadge(userMail []byte) []byte {
 	if !enabled {
-		return svg.GetBullet("")
+		return svg.Offline
 	}
+
+	mail := string(userMail)
 
 	// Bots always online
 	if bots[mail] {
@@ -111,20 +117,20 @@ func getStatusBadge(mail string) string {
 
 	switch slack.GetStatus(mail) {
 	case slack.STATUS_OFFLINE:
-		return svg.GetCircle()
+		return svg.Offline
 	case slack.STATUS_ONLINE:
 		return svg.GetBullet(COLOR_ONLINE)
 	case slack.STATUS_DND:
 		return svg.GetBullet(COLOR_DND)
 	case slack.STATUS_DND_OFFLINE:
-		return svg.GetDND()
+		return svg.DND
 	case slack.STATUS_VACATION:
-		return svg.GetAirplane()
+		return svg.Vacation
 	case slack.STATUS_ONCALL:
-		return svg.GetPhone()
+		return svg.OnCall
 	case slack.STATUS_DISABLED:
-		return svg.GetBullet("")
+		return svg.Empty
 	default:
-		return svg.GetBullet("")
+		return svg.Empty
 	}
 }
